376dfeb8c8e283a45bde8fa9a646d04a15cfad23
rohanmohapatra <rohanironcrest14@gmail.com>
2019-12-15 12:40:47 +0530

    Updates

---
+++ precache-manifest.d00ba460e5a6d6645e93bc34926e8744.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a54b275a9ee4180d65e6ad08f706a97",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5d28bafcbe300f56ed6b",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b9d1305842bbf8690fe0",
+    "url": "/portfolio/static/js/2.ac7389bd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.ac7389bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d28bafcbe300f56ed6b",
+    "url": "/portfolio/static/js/main.e0153bba.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
